522d52ec6acb8b40ef9a80d0737f0138a1b3ecd4
Aman Kasliwal <aman.kasliwal@juspay.in>
2018-08-06 01:01:23 +0530

    adding clusters

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 const express = require('express')
-const app = express()
+// const app = express()
 const models = require('./db/models');
 const port = process.env.PORT || 8081;
 const env = process.env.NODE_ENV || "development";
 const contactRoute = require("./routes/contact")
 var bodyParser = require('body-parser');
 var authMiddleware = require('./middlewares/auth');
+const cluster = require("cluster");
 
-startServer = () => {
-  models.sequelize.sync().then(function () {
-    app.listen(port, function () {
-      console.info("Contacts server listening on port " + port + " in " + env + " mode");
-    })
-  });
-};
-
-initMiddlewares = () => {
+initMiddlewares = (app) => {
   app.use(bodyParser.urlencoded({ extended: true }));// configure app to use bodyParser()
   app.use(bodyParser.json());
   app.use('/contacts', authMiddleware);
 
 }
 
-initRoutes = () => {
+initRoutes = (app) => {
   app.get('/', (req, res) => res.send('UP'));
   app.use("/contacts", contactRoute);
 }
 
-startServer();
-initMiddlewares();
-initRoutes();
-
+startCluster = () => {
+  if(cluster.isMaster) {
+    let cpuCount = require('os').cpus().length;
+    for (let i = 0; i < cpuCount; i += 1) {
+      console.log("Forking worker ", i);
+      cluster.fork();
+    }
+    models.sequelize.sync();
+  } else {
+    var app = express();
+    initMiddlewares(app);
+    initRoutes(app);
+    app.disable('x-powered-by');
+    app.listen(port, function () {
+      console.info("Contacts server listening on port " + port + " in " + env + " mode");
+    })
+  }
+};
+startCluster();
